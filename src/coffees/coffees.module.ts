97ef11d5c29784f99a1410beaafec7ba48f6c998
Suhas Jagadeesh <stj@Suhass-Air.fios-router.home>
2024-12-30 10:45:56 -0500

    Chapter 4 complete

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -4,11 +4,16 @@ import { CoffeesService } from './coffees.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
+import { COFFEE_BRANDS } from './coffees.constants';
 
 // every Module can contain 4 properties: controllers, exports, imports, providers
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
-  providers: [CoffeesService],
+  providers: [
+    CoffeesService,
+    { provide: COFFEE_BRANDS, useFactory: () => ['Starbucks', 'Dunkin'] },
+  ],
+  exports: [CoffeesService],
 })
 export class CoffeesModule {}
